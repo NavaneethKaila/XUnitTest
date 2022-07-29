28392c72e67229c7cd01aa8fa06e2d504fe08b1c
NavaneethKaila <navaneeth.kaila@gmail.com>
2022-07-29 10:08:37 +0530

    Add project files.

---
+++ XUnitTest/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using ClassLibrary;
+using System;
+
+namespace XUnitTest
+{
+    public class Class1
+    {
+        LengthCounter obj = new LengthCounter();
+        [Fact]
+        public void NullCheck()
+        {
+            Assert.Equal(0, obj.counter(""));
+        }
+        [Fact]
+        public void StringCheck()
+        {
+            Assert.Equal(3, obj.counter("123"));
+        }
+
+        [Fact]
+        public void NumberCheck()
+        {
+            Assert.Equal(12, obj.counter("123456789010"));
+        }
+    }
+}
